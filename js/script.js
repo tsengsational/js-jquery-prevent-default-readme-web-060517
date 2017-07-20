57171f9cb865351836853595c392b646f11968b1
github username <github email address>
2017-07-20 16:04:44 -0400

    Done.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-  
+
   // browser refreshes on submit
-  $('form').on('submit', function(){
-    var name = $('#name').val();
-    $("#hello").text("Hello, " + name);
-  });
+  // $('form').on('submit', function(){
+  //   var name = $('#name').val();
+  //   $("#hello").text("Hello, " + name);
+  // });
 
   //examine event object
   // $('form').on('submit', function(event){
@@ -12,13 +12,13 @@ $(document).ready(function(){
   //   $("#hello").text("Hello, " + name);
   //   debugger;
   //   event.preventDefault();
-  // });
+  });
 
   // stop page refresh
-  // $('form').on('submit', function(event){
-  //   var name = $('#name').val();
-  //   $("#hello").text("Hello, " + name);
-  //   event.preventDefault();
-  // });
+  $('form').on('submit', function(event){
+    var name = $('#name').val();
+    $("#hello").text("Hello, " + name);
+    event.preventDefault();
+  });
 
 });
